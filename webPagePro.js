abe22f2b4ca8970043c46773217bf2862e75d05d
mdafsha9 <mdafsha9.gmail.com>
2021-12-04 00:51:07 -0600

    web page propewrties

---
+++ webPagePro.js
@@ -0,0 +1,35 @@
+//web page properties
+//Finding an HTML element by HTML object collections & properties
+
+const a = document;
+console.log(a);
+
+const b = document.title;
+console.log(b);
+
+const c = document.head;
+console.log(c);
+
+const d = document.doctype;
+console.log(d);
+
+const e = document.body;
+console.log(e);
+
+//right now we dont have images
+const f = document.images;
+console.log(f);
+
+//right now we don't have forms
+const g = document.forms;
+console.log(g);
+
+const h = document.URL;
+console.log(h);
+
+const i = document.baseur;
+console.log(i);
+
+//right now we dont have domain
+const j = document.domian;
+console.log(j);
