f64bd00e6f65854627798bfb7f4db47f3ef51e43
mdafsha9 <mdafsha9.gmail.com>
2021-12-06 00:58:16 -0600

    change text and HTML

---
+++ setDom.js
@@ -0,0 +1,18 @@
+//innerText
+//innerHTML
+//setAttributes
+//Attributes
+//.value
+//.name
+//removeAttributes
+
+//innerText
+let h = document.querySelector('h1');
+h.innerText = "Document Object Model";
+console.log(h);
+
+//innerHTML
+
+let header = document.querySelector('.container, .header');
+header.innerHTML = "<h2>innerHTML</h2>";
+console.log(header);
