3bf6ffcbc901677cbf94c3ffd98e1506b41dba72
mdafsha9 <mdafsha9@gmail.com>
2022-02-24 04:55:50 -0600

    change the style of an element

---
+++ 05.js
@@ -0,0 +1,7 @@
+//change the styles of an elements
+
+const header = document.getElementById("header");
+header.style.color = "blue";
+header.style.backgroundColor = "yellow";
+header.style.textAlign = "center";
+header.style.padding = "20px";
