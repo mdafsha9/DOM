d04790fb3f07de7132078df03b48cba9379ff5bf
mdafsha9 <mdafsha9@gmail.com>
2022-02-25 00:46:57 -0600

    get and set attribute

---
+++ 06.js
@@ -0,0 +1,8 @@
+//get and set attribute
+
+
+const header = document.querySelector("#header");
+console.log(header.getAttribute("id"));
+
+header.setAttribute("class", "header1");
+console.log(header.getAttribute("class"));
