58ec497412c8ce6e1bf1f1f992ad7da665a947aa
mdafsha9 <mdafsha9@gmail.com>
2022-02-25 01:33:47 -0600

    get multiple elements using getElementByclassNAme & querySelectorAll

---
+++ 07.js
@@ -0,0 +1,9 @@
+//get multiple elements using element by class name
+//get multiple elements using querySelectorAll
+
+const lists = document.getElementsByClassName("methods");
+console.log(lists);
+console.log(lists[1]);
+
+const listItems = document.querySelectorAll(".methods");
+console.log(listItems);
