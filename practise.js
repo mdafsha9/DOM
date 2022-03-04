b88fbd0cb507a5c9de9594784dc15387835a72d4
mdafsha9 <mdafsha9@gmail.com>
2022-03-04 04:32:42 -0600

    innerHTML

--- practise.js
+++ practise.js
@@ -12,7 +12,7 @@ console.log(node);
 const node1 = document.querySelectorAll(".methods");
 console.log(node1[1]);
 
-
+//iterate elements
 for(let i=0; i<node1.length; i++){
   console.log(node1[i]);
 }
@@ -21,8 +21,15 @@ for(let node2 of node1){
   console.log(node2);
 }
 
-
+//get & set attributes
 const anchor = document.querySelector("#anchor a");
 console.log(anchor.getAttribute("href"));
 anchor.setAttribute("href", "https://www.w3schools.com");
 console.log(anchor.getAttribute("href"));
+
+//innerHTML
+
+const answer = document.querySelector(".container");
+console.log(answer.innerHTML);
+answer.innerHTML = "<h1>Changed innerHTML</h1>";
+console.log(answer.innerHTML);
