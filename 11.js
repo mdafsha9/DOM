9daa6fa281bd29b091bc03b5c5362a89faf51b11
mdafsha9 <mdafsha9@gmail.com>
2022-03-11 05:32:56 -0600

    classList, add and remove, toggle

---
+++ 11.js
@@ -0,0 +1,8 @@
+//classList, add and remove, toggle
+
+const sectionTodo = document.querySelector(".section-todo");
+console.log(sectionTodo.classList);
+sectionTodo.classList.add("bg-dark");
+sectionTodo.classList.add("fontStyling");
+sectionTodo.classList.remove("fontStyling");
+sectionTodo.classList.toggle("fontStyling");
